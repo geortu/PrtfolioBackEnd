94689f9b6bc01b302aa2e5586841d9a8669a8cbe
geortu <jorge.arturo.biazzo@gmail.com>
2022-12-28 13:28:48 -0300

    Ultimo

--- src/main/java/com/portfolio/portfolioweb/conficuracion/CORSConfig.java
+++ src/main/java/com/portfolio/portfolioweb/conficuracion/CORSConfig.java
@@ -7,11 +7,22 @@ package com.portfolio.portfolioweb.conficuracion;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@EnableWebMvc
 @Configuration
-public class CORSConfig {
-    @Bean
+public class CORSConfig implements WebMvcConfigurer {
+    
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT","DELETE");
+
+    }
+    /*@Bean
     public WebMvcConfigurer CORSConfigurer(){
         return new WebMvcConfigurer() {
             @Override
@@ -21,6 +32,6 @@ public class CORSConfig {
                         .allowedMethods("GET", "POST", "PUT","DELETE");
             }
         };
-    }
+    }*/
     
 }
